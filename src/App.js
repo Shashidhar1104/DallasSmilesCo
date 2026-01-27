6e2c1b1a64292f6bcf01c4b76ab5ed27cbbfddb5
Shashidhar Tatikondala <shashitatikondala2@gmail.com>
2026-01-27 12:52:56 +0530

    Clean install and router fix

--- src/App.js
+++ src/App.js
@@ -1,27 +1,23 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Footer from "./components/Footer";
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="min-h-screen bg-white text-gray-900">
-        <Navbar />
+    <div className="min-h-screen bg-white text-gray-900">
+      <Navbar />
 
-        <Routes>
-          <Route path="/" element={<Home />} />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/professionals" element={<Home />} />
+        <Route path="/offices" element={<Home />} />
+        <Route path="/login" element={<Home />} />
+        <Route path="/signup" element={<Home />} />
+      </Routes>
 
-          {/* Temporary placeholder routes so navbar links work */}
-          <Route path="/professionals" element={<Home />} />
-          <Route path="/offices" element={<Home />} />
-          <Route path="/login" element={<Home />} />
-          <Route path="/signup" element={<Home />} />
-        </Routes>
-
-        <Footer />
-      </div>
-    </BrowserRouter>
+      <Footer />
+    </div>
   );
 }
 
